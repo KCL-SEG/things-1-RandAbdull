ada39e94c08e7a62a21f7af044af7353c4dd87ff
Rand Albahusain <rand.albahusain@kcl.ac.uk>
2022-12-05 15:23:40 +0000

    Fix errors

--- things/admin.py
+++ things/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from .models import Thing
-
-@admin.register(Thing)
-class UserAdmin(admin.ModelAdmin):
-    """Configuration of the admin interface for users."""
-
-    list_display = [
-        'name', 'description', 'quantity',
-    ]
+# from django.contrib import admin
+# from .models import Thing
+#
+# @admin.register(Thing)
+# class UserAdmin(admin.ModelAdmin):
+#     """Configuration of the admin interface for users."""
+#
+#     list_display = [
+#         'name', 'description', 'quantity',
+#     ]
--- things/models.py
+++ things/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-class Thing(AbstractUser):
+class Thing(models.Model):
     name = models.CharField(unique = True, max_length=30, blank=False)
     description = models.CharField(unique = False,max_length=120, blank=True)
-    quantity = models.IntegerField(unique = False,validators=[MaxValueValidator(100), MinValueValidator(0)])
+    quantity = models.IntegerField(unique = False,validators=[ MinValueValidator(0), MaxValueValidator(100)])
